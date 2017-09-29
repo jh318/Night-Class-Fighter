6456e2cbaf2ab7e507a4f88bf8fcff161b731792
Joseph Bedsole <jbedsole@dmi-intertech.org>
2017-09-29 18:42:48 -0500

    Changed the navigation/button script a bit.

--- Assets/FightingGame/Scripts/CharSelectController.cs
+++ Assets/FightingGame/Scripts/CharSelectController.cs
@@ -13,6 +13,8 @@ public class CharSelectController : MonoBehaviour {
 
     Selectable newSelection1;
     Selectable newSelection2;
+    Selectable prevSelect1;
+    Selectable prevSelect2;
 
     [Header("Player Selection Indicators")]
     public Transform p1Indicator;
@@ -22,8 +24,16 @@ public class CharSelectController : MonoBehaviour {
     public Animator p1Anim;
     public Animator p2Anim;
 
+    public bool p1Ready = false;
+    public bool p2Ready = false;
 
-void Start ()
+    // To-Do
+    // Selection(Transfer to ready state after selecting a character);
+    // Make sound when moving or selecting(Play animations);
+    // Make a previous newSelection to allow for sounds to play during selection changes;
+    // Select Character, change sprite for selection, gray out selection, transfer to ready(give option to go back with (B)), and load character data;
+
+    void Start ()
     {
         p1 = defaultButton;
         p2 = defaultButton;
@@ -44,13 +54,23 @@ void Start ()
         float p2Vert = Input.GetAxisRaw("Vertical2");
         Vector2 p2Dir = new Vector2(p2Hori, p2Vert);
 
-        // Movement and Selection
-        // Move the p1 or p2 indicator depending on the position navigated to && Make sound when moving or selecting;
-        // Make a previous newSelection to allow for sounds to play during selection changes;
-
         // Initialize the Selectable and look for other selectables in the direction of the players' choice;
-        Selectable newSelection1 = p1.FindSelectable(p1Dir);
-        Selectable newSelection2 = p2.FindSelectable(p2Dir);
+        newSelection1 = p1.FindSelectable(p1Dir);
+        newSelection2 = p2.FindSelectable(p2Dir);
+
+        //----------------------------------------------------------------------------------------
+
+        if (newSelection1 != null)
+        {
+            p1Indicator.transform.position = newSelection1.transform.position;
+        }
+
+        if (newSelection2 != null)
+        {
+            p2Indicator.transform.position = newSelection2.transform.position;
+        }
+
+        //----------------------------------------------------------------------------------------
 
         if (newSelection1 != p1 && newSelection1 != null)
         {
@@ -63,6 +83,49 @@ void Start ()
             p2 = newSelection2;
             Debug.Log("Moved To The P2 Spot.");
         }
-	}
+
+        //----------------------------------------------------------------------------------------
+
+        CheckSelectionChange();
+
+        //----------------------------------------------------------------------------------------
+
+        if (Input.GetButton("AButton") && !p1Ready)
+        {
+            // p1Ready = true;
+            newSelection1.Select();
+            Debug.Log("I'm Selecting YO!");
+        }
+        //else if (Input.GetButtonDown("BackButton") && p1Ready)
+        //{
+        //    p1Ready = false;
+        //}
+
+        if (Input.GetButtonDown("AButton2") && !p1Ready)
+        {
+            p1Ready = true;
+
+        }
+        //else if (Input.GetButtonDown("BackButton2") && p1Ready)
+        //{
+        //    p1Ready = false;
+        //}
+
+    }
+
+    void CheckSelectionChange()
+    {
+        if (prevSelect1 != newSelection1 && prevSelect1 != null)
+        {
+            prevSelect1 = newSelection1;
+            // Change music and Animation
+        }
+
+        if (prevSelect2 != newSelection2 && prevSelect2 != null)
+        {
+            prevSelect2 = newSelection2;
+            // Change music and Animation
+        }
+    }
 
 }
