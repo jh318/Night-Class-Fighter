d9c02509a222d1902b404d8ea30484e625bb7330
Evan Kiser <ekiser247@gmail.com>
2017-09-28 23:31:51 -0500

    Made the Health bars move and scale

--- Assets/FightingGame/Scripts/TimerScript.cs
+++ Assets/FightingGame/Scripts/TimerScript.cs
@@ -8,19 +8,29 @@ public class TimerScript : MonoBehaviour {
     public int time;
     bool timesUp = false;
     public Text TimeUI;
+    public GameObject TimesUpUI;
 
 	void Start () {
         StartCoroutine("Timer");
 	}
 
+    private void Update()
+    {
+        if (timesUp == true)
+        {
+            TimesUpUI.gameObject.SetActive(true);
+        }
+    }
+
     IEnumerator Timer()
     {
         yield return new WaitForEndOfFrame();
-        for (int i = time; i >= 0; i--)
+        for (int i = 0; time >= i; time--)
         {
             yield return new WaitForSecondsRealtime(1);
-            TimeUI.text = "" + i;
+            TimeUI.text = "" + time;
         }
+        TimeUI.text = "0";
         timesUp = true;
     }
 }
