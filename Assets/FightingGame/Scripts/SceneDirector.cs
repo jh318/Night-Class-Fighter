cdebc80f66f1c52666e2537e4eccccbfccdb2f61
David Jones <Jones_David_S@yahoo.com>
2017-09-29 21:13:34 -0500

    adds audio man, scene director

---
+++ Assets/FightingGame/Scripts/SceneDirector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneDirector : MonoBehaviour
+{
+    SceneDirector instance;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void Title ()
+    {
+        SceneManager.LoadScene("Title Screen");
+    }
+    public void MainMenu ()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
