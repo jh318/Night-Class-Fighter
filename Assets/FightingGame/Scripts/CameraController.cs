99a27ae6c919bbcd2180255e68d68ab4ecf230d2
Chris Matthews <cmatthews@dmi-intertech.org>
2017-10-19 23:22:04 -0500

    started working on camera controler

---
+++ Assets/FightingGame/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public static CameraController instance;
+
+    public GameObject player1;
+    public GameObject player2;
+
+    
+
+	void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+    
+    // Use this for initialization
+	void Start ()
+    {
+        float middle = (player2.transform.position.x - player1.transform.position.x) / 2;
+        transform.position = new Vector3(player2.transform.position.x - middle, 2.18f, -4.17f);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
