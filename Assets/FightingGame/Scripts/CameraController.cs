529cb5178c1a142fdbfa492e24eb63038161be0c
Chris Matthews <cmatthews@dmi-intertech.org>
2017-10-20 23:33:28 -0500

    did some camera work

--- Assets/FightingGame/Scripts/CameraController.cs
+++ Assets/FightingGame/Scripts/CameraController.cs
@@ -6,8 +6,11 @@ public class CameraController : MonoBehaviour {
 
     public static CameraController instance;
 
-    public GameObject player1;
-    public GameObject player2;
+    [SerializeField]
+    private GameObject player1;
+
+    [SerializeField]
+    private GameObject player2;
 
     
 
@@ -20,15 +23,51 @@ public class CameraController : MonoBehaviour {
     }
     
     // Use this for initialization
-	void Start ()
+	IEnumerator Start ()
     {
+        yield return new WaitForEndOfFrame();
+
+        PlayerController[] playerController = FindObjectsOfType(typeof(PlayerController)) as PlayerController[];
+
+        foreach (PlayerController pc in playerController)
+        {
+            if(pc.playerNumber == 0)
+            {
+                player1 = pc.gameObject;
+            }
+            else if (pc.playerNumber == 1)
+            {
+                player2 = pc.gameObject;
+            }
+        }
+
+       
+
         float middle = (player2.transform.position.x - player1.transform.position.x) / 2;
         transform.position = new Vector3(player2.transform.position.x - middle, 2.18f, -4.17f);
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        float distance = (player1.transform.position - player2.transform.position).x;
+        float middle = distance / 2;
+
+        //if player 2 is on the right, distance will be negative
+        if (distance < 0)
+        {
+            Debug.Log("Player2 is on the right");
+            transform.position = new Vector3(player2.transform.position.x + middle, transform.position.y, transform.position.z);
+        }
+        else if(distance > 0)//means player1 is on the right
+        {
+            Debug.Log("Player1 is on the right");
+            transform.position = new Vector3(player2.transform.position.x + middle, transform.position.y, transform.position.z);
+        }
+        else if(distance == 0)
+        {
+            transform.position = new Vector3(player2.transform.position.x , transform.position.y, transform.position.z);
+        }
 	}
 }
